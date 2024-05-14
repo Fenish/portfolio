345b8fc7b554b0466e687d4b1c569a00b59f77a6
Fenish <sohretalhadev@outlook.com>
2024-05-14 18:06:11 +0300

    Remove unnecessary files

--- authenticated-todo/src/utils/textUtils.ts
+++
@@ -1,8 +0,0 @@
-import { randomBytes } from 'crypto';
-
-export function generateCallId() {
-	const bytes = randomBytes(6);
-	const hexString = bytes.toString('hex');
-	const callId: string = `${hexString}-9`;
-	return callId;
-}
