bcb8215d408a1ba09b0058933b5f95066ee0fe7f
Fenish <sohretalhadev@outlook.com>
2024-05-17 19:49:12 +0300

    Remove project from repository

--- authenticated-todo/src/decorators/auth/auth.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { applyDecorators, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth } from '@nestjs/swagger';
-
-import { ProtectedRouteGuard } from '../../api/authentication/guards/protected.guard';
-
-export function ProtectedRoute() {
-	return applyDecorators(UseGuards(ProtectedRouteGuard), ApiBearerAuth());
-}
